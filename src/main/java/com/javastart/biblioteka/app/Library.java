822fe7e52c9aecab140feb3402d9870f6f9bfc39
jarhuba <jarhuba@gmail.com>
2018-10-16 13:55:32 +0200

    Temat 14 v 0.6

--- src/main/java/com/javastart/biblioteka/app/Library.java
+++ src/main/java/com/javastart/biblioteka/app/Library.java
@@ -1,30 +1,25 @@
 package com.javastart.biblioteka.app;
 
 import com.javastart.biblioteka.data.Book;
+import com.javastart.biblioteka.utils.DataReader;
 
 public class Library {
 
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
         Book[] books = new Book[1000];
+        DataReader dataReader = new DataReader();
 
-
-        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010, 296, "Greg", "9788373271890");
-        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009, 352, "Helion",
-                "9788324620845");
-        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide", "Bert Bates, Katherine Sierra",
-                2008, 851, "McGraw-Hill Osborne Media", "9780071591065");
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println("Wprowadź nową książkę: ");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
+
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
 
-        books[3] = new Book(books[2]);
-
-        books[3].printInfo();
-
-        System.out.println("System może przechowywać do " + books.length + " książek.");
+        System.out.println("System może przechowywać do " + books.length + " książek");
     }
 }
