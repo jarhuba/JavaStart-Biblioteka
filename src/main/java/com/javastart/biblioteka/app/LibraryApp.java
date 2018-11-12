497cbbd81d2437f9bb195f30f7c108cc684d4afb
jarhuba <jarhuba@gmail.com>
2018-11-12 21:31:34 +0100

    Temat 27 Rekurencja v 0.9.2.2 recursive

--- src/main/java/com/javastart/biblioteka/app/LibraryApp.java
+++ src/main/java/com/javastart/biblioteka/app/LibraryApp.java
@@ -6,9 +6,9 @@ import com.javastart.biblioteka.utils.DataReader;
 public class LibraryApp {
 
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9.2.2";
+        final String appName = "Biblioteka v0.9.2.2 recursive";
         System.out.println(appName);
         LibraryControl libControl = new LibraryControl();
-        libControl.controlLoop();
+        libControl.controlLoop(null);
     }
 }
