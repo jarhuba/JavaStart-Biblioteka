0ae4a461ea621f6afd4781cfc3b3db047069d129
jarhuba <jarhuba@gmail.com>
2018-11-12 20:38:31 +0100

    Temat 18 v 0.8.2

--- src/main/java/com/javastart/biblioteka/data/Publication.java
+++ src/main/java/com/javastart/biblioteka/data/Publication.java
@@ -9,24 +9,25 @@ public class Publication {
     public int getYear() {
         return year;
     }
-
     public void setYear(int year) {
         this.year = year;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getPublisher() {
         return publisher;
     }
-
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    protected Publication(int year, String title, String publisher) {
+        setYear(year);
+        setTitle(title);
+        setPublisher(publisher);
     }
 }
